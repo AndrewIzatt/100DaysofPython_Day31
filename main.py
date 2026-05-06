9ab0ab9d67f3d99e5f0405653d43d5295626e69f
Andrew Izatt <izatt.andrew@gmail.com>
2026-05-05 18:25:18 -0600

    Add words to flashcard

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import PhotoImage
 
 BACKGROUND_COLOR = "#B1DDC6"
+FONT_NAME = "Ariel"
 
 # Initiate window
 window = Tk()
@@ -12,6 +13,8 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(window, width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_image = PhotoImage(file="/Users/meno/Repos/100DaysofPython_Day31/images/card_front.png")
 canvas.create_image(400, 263, image=front_image)
+top_text = canvas.create_text(400, 150, text="Language", fill="black", font=(FONT_NAME, 40, "italic"))
+bottom_text = canvas.create_text(400, 263, text="Word", fill="black", font=(FONT_NAME, 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Buttons
